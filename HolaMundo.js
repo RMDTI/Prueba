bcc52c28c383e4ba0cec63c03adde74ebdc2ccd6
RMDTI <35747483+RMDTI@users.noreply.github.com>
2018-01-25 22:21:18 -0600

    Update HolaMundo.js

--- HolaMundo.js
+++ HolaMundo.js
@@ -2,7 +2,8 @@ console.log("Hola mundo");
 <<<<<<< HEAD
 console.log("HOLA2");
 console.log("HOLA2");
+console.log("");
 =======
 console.log("Hola mundo");
 console.log("Hola mundo");
->>>>>>> bug
+>>>>>>> bug2
